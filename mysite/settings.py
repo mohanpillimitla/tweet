8c6b95107fb16b1b545170af026fd91bcc0d267c
mohan_1210 <mohanpillimitla143@gmail.com>
2019-03-06 14:00:06 +0530

    static files

--- mysite/settings.py
+++ mysite/settings.py
@@ -130,11 +130,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_ROOT='staticfiles'
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS= (os.path.join(BASE_DIR,'static'),)
 
 SECURE_SSL_REDIRECT=(os.environ['SECURE_SSL_REDIRECT']=='True')
 
