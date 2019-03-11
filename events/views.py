042e37064087d3a475089025f680b751552245d8
mohan_1210 <mohanpillimitla143@gmail.com>
2019-03-11 19:54:14 +0530

    helo

--- events/views.py
+++ events/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def eventView(request):
-	return render(request,'events/event_list.html',{})
+	return render(request,'events/event_list.html')
